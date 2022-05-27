feaeb0d52b2248828f0eca5d5706157eb3c3f9bf
Rajesh Kumar <rajesh.kumar_cs19@gla.ac.in>
2022-05-27 12:30:36 +0530

    Update

--- src/components/Pages/Search/Search.js
+++ src/components/Pages/Search/Search.js
@@ -11,7 +11,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import CustomPagination from "../../Pagination/CustomPagination";
-import SingleContent from "../../components/SingleContent/SingleContent";
+import SingleContent from "../../SingleContent/SingleContent";
 
 const Search = () => {
   const [type, setType] = useState(0);
@@ -32,9 +32,9 @@ const Search = () => {
   const fetchSearch = async () => {
     try {
       const { data } = await axios.get(
-        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
-          process.env.REACT_APP_API_KEY
-        }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+        `https://api.themoviedb.org/3/search/${
+          type ? "tv" : "movie"
+        }?api_key=fe7f525d019d7186e76683b4233c4a0c&language=en-US&query=${searchText}&page=${page}&include_adult=false`
       );
       setContent(data.results);
       setNumOfPages(data.total_pages);
